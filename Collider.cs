1167fab6ada35a6c31112223757eccd0e6a3177c
Michele Rossi <mikymod@gmail.com>
2021-07-21 15:56:26 +0200

    add collider (abstract)

---
+++ Collider.cs
@@ -0,0 +1,25 @@
+﻿using OpenTK;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopDownShooterAIV
+{
+    abstract class Collider
+    {
+        public GameObject GameObject;
+
+        public Vector2 Offset;
+        public Vector2 Position { get => GameObject.Position + Offset; }
+
+        public Collider(GameObject owner)
+        {
+            GameObject = owner;
+            Offset = Vector2.Zero;
+        }
+
+        public abstract bool Collides(Collider collider, ref Collision collisionInfo);
+    }
+}
