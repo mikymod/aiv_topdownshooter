aa73e915fe57cb321be0f3d96477a085e541299f
Michele Rossi <mikymod@gmail.com>
2021-07-21 15:55:11 +0200

    add collision

---
+++ Collision.cs
@@ -0,0 +1,18 @@
+﻿using OpenTK;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopDownShooterAIV
+{
+    enum CollisionType { None, RectsIntersection, CirclesIntersection, CircleRectIntersection }
+
+    class Collision
+    {
+        public GameObject Collider;
+        public Vector2 Delta;
+        public CollisionType Type;
+    }
+}
