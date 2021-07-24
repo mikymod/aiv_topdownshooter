405bac97300cdb82bbcbc2cdd7a52839902ce6e9
Michele Rossi <mikymod@gmail.com>
2021-07-24 18:02:56 +0200

    add state machine

---
+++ FSM/State.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopDownShooterAIV.FSM
+{
+    public abstract class State
+    {
+        StateMachine sm;
+
+        public State(StateMachine sm)
+        {
+            this.sm = sm;
+        }
+
+        public virtual void OnEnter() { }
+        public virtual void OnLogic() { }
+        public virtual void OnExit() { }
+        
+    }
+}
---
+++ FSM/StateMachine.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopDownShooterAIV.FSM
+{
+    public class StateMachine
+    {
+        public GameObject Owner { get; }
+
+        private Dictionary<String, State> states;
+        private State current;
+        private bool initialized;
+
+        public StateMachine(GameObject owner)
+        {
+            this.Owner = owner;
+            states = new Dictionary<string, State>();
+            initialized = false;
+        }
+
+        public void AddState(String key, State state) => states[key] = state;
+        
+
+        public void ChangeState(String key)
+        {
+            current?.OnExit();
+            current = states[key];
+            current.OnEnter();
+        }
+
+        public void SetInitialState(String key)
+        {
+            current = states[key];
+            initialized = true;
+        }
+
+        public void Update()
+        {
+            if (!initialized)
+            {
+                return;
+            }
+
+            current.OnLogic();
+        }
+    }
+}
